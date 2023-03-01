22cceabb74d8caf36f4a112f38a4d2127d042080
behradk90@gmail.com <behradk90@gmail.com>
2023-03-01 14:35:40 +0000

    Update

---
+++ models/schema.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const CocktailRecipeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    desciption: {
+        type: String
+    },
+    recipe: {
+        type: String,
+        required: true
+    },
+    glass: {
+        type: String
+    },
+    ice: {
+        type: String
+    },
+    garnish: {
+        type: String
+    }
+});
+
+module.exports = Cocktail = mongoose.model('cocktail', CocktailRecipeSchema);
