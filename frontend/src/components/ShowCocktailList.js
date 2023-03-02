29eac2c381ac58b9a38912cde5f363c8d83a9636
behradk90@gmail.com <behradk90@gmail.com>
2023-03-02 12:59:37 +0000

    ShowCocktailList component written

---
+++ frontend/src/components/ShowCocktailList.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import RecipeCard from './RecipeCard';
+
+function ShowCocktailList() {
+    const [cocktails, setCocktails] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:8080/cocktails')
+            .then((res) => {
+                setCocktails(res.data);
+            })
+            .catch((err) => {
+                console.log('Error from ShowCocktailList.')
+            });
+    }, []);
+
+    const cocktailList =
+        cocktails.length === 0
+            ? 'There is no cocktail record!'
+            : cocktails.map((cocktail, k) => <RecipeCard cocktail={cocktail} key={k} />)
+
+    return (
+        <div className="ShowCocktailList">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <br />
+                        <h2 className="display-4 text-center">Book List</h2>
+                    </div>
+
+                    <div className="col-md-11">
+                        <Link
+                            to='/create-cocktail'
+                            className="btn btn-outline-warning float-right"
+                        >
+                            + Add New Cocktail
+                        </Link>
+                        <br />
+                        <br />
+                        <br />
+                    </div>
+                </div>
+                <div className="list">{cocktailList}</div>
+            </div>
+        </div>
+    );
+};
+
+module.exports = ShowCocktailList;
