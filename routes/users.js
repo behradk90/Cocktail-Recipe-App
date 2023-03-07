1618d70901099cde83755e7119310dfeff65ee1b
behradk90@gmail.com <behradk90@gmail.com>
2023-03-07 11:20:00 +0000

    Authorization middleware established in user route

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { signUp, signIn } = require('../controllers/auth.controller')
+const { signUp, signIn } = require('../controllers/auth.controller');
+const verifyToken = require('../middleware/authJWT');
+const user = require('../models/user');
 
 router.post("/register", signUp, function (req, res) {
 
@@ -9,5 +11,25 @@ router.post("/register", signUp, function (req, res) {
 router.post("/login", signIn, function (req, res) {
 
 });
+
+router.get('/admin', verifyToken, function (req, res) {
+    if (!user) {
+        res.status(403)
+            .send({
+                message: "Invalid JWT token"
+            });
+    }
+    if (req.user == "admin") {
+        res.status(200)
+            .send({
+                message: "Congratulations! Admin privileges pending..."
+            });
+    } else {
+        res.status(403)
+            .send({
+                message: "Unauthorized access"
+            })
+    }
+})
 
 module.exports = router;
