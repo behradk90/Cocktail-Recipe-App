1fc7de210f5630ab4cb1a241e739e6a32d973449
behradk90@gmail.com <behradk90@gmail.com>
2023-03-08 11:19:09 +0000

    Beginning of Home page

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Home() {
+    return (
+        <div className='d-flex justify-content-center'>
+            <h1>Cocktail-App</h1>
+            <Link
+                to='cocktail-list'
+                className='btn btn-info center'
+            >
+                Enter
+            </Link>
+        </div>
+    )
+}
+
+
+export default Home;
