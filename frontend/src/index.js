2ea7b6c5f43ce50f2a6e9be1670ef82657b23c2a
behradk90@gmail.com <behradk90@gmail.com>
2023-03-21 14:52:24 +0000

    useAuthContext hook created

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { AuthContextProvider } from './Context/LoginContext';
+import { AuthContextProvider } from './Context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
